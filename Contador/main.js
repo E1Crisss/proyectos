5673298f31254126f50c78ae850bea8bbbf1302b
E1Crisss <cristiangau@hotmail.com>
2022-07-09 15:12:24 -0300

    codigo corregiendo el valor de 0 a negativo ahora faltaria darle stylo

--- Contador/main.js
+++ Contador/main.js
@@ -1,32 +1,32 @@
-$decrease = document.querySelector("#decrease"),
-$reset = document.querySelector('#reset'),
-$increase = document.querySelector("#increase");
-const $div = document.querySelector("div");
+const $div = document.querySelector("div"),
+  $decrease = document.querySelector("#decrease"),
+  $reset = document.querySelector("#reset"),
+  $increase = document.querySelector("#increase");
 
 let num = 0;
 
-  const incrementClick = () => {
-    updateDisplay(++num);
-}
-  const decreaseClick = () => {
+const incrementClick = () => {
+  updateDisplay(++num);
+};
+const decreaseClick = () => {
+  if (num >= 1) {
     updateDisplay(--num);
-}
+  }
+};
 
 const resetCounter = () => {
-    num = 0;
-    updateDisplay(num);
-  }
-  
-  const $span = document.createElement("span");
-const updateDisplay = () =>{
+  num = 0;
+  updateDisplay(num);
+};
 
+const $span = document.createElement("span");
+$span.innerHTML = num;
+$div.appendChild($span);
+
+const updateDisplay = () => {
   $span.innerHTML = num;
-  $div.appendChild($span);
-}
+};
 
-  
-  
 $increase.addEventListener("click", incrementClick);
 $decrease.addEventListener("click", decreaseClick);
-$reset.addEventListener('click', resetCounter)
-
+$reset.addEventListener("click", resetCounter);
