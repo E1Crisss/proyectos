13694a88a1f7d9553b18a548e80544ca5ce52dff
E1Crisss <cristiangau@hotmail.com>
2022-07-08 23:29:44 -0300

    iniciando counter

---
+++ Contador/main.js
@@ -0,0 +1,17 @@
+const $div = document.querySelector("div"),
+  $decrease = document.querySelector("#decrease"),
+  // $reset = document.querySelector('#reset'),
+  $increase = document.querySelector("#increase");
+
+  
+  const incre = () =>{
+      let counter = ;
+    }
+    
+    let num = 11;
+const $span = document.createElement("span");
+$span.innerHTML = num;
+
+$div.appendChild($span);
+
+$increase.addEventListener("click", incre);
